9a4f7836151e08ec8d8027eed6769404a0ccb28e
Nuri Akman <nuriakman@gmail.com>
2025-08-28 18:56:31 +0300

    Veri çekme tamam

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,28 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+  <q-page>
+
+    <pre>
+      {{ personeller }}
+    </pre>
+
   </q-page>
 </template>
 
+
 <script setup>
-//
+import { api } from 'boot/axios'
+import { onMounted } from 'vue'
+import { ref } from 'vue'
+
+const personeller = ref([])
+
+async function getPersonel(params = {}) {
+  const { data } = await api.get('http://localhost:8000/api/personel', { params })
+  console.log(data)
+  personeller.value = data
+}
+
+onMounted(() => {
+  getPersonel()
+})
 </script>
